fe5e9cd928aaca19ae0b8cfa8a060207aea451ab
ALİ ENES <ae.ersozlu@gmail.com>
2024-05-01 15:50:20 +0300

    ilk adım tamamlandı

---
+++ src/products.js
@@ -0,0 +1,62 @@
+
+import product1 from "./assets/1.jpg";
+import product2 from "./assets/2.jpg";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.jpg";
+import product6 from "./assets/6.jpg";
+import product7 from "./assets/7.jpg";
+import product8 from "./assets/8.jpg";
+
+
+
+export const PRODUCTS = [
+    {
+      id: 1,
+      productName: "Siyam kedisi",
+      price: 13.0,
+      productImage: product1,
+    },
+    {
+      id: 2,
+      productName: "Devon Rex",
+      price: 15.99,
+     productImage: product2,
+    },
+    {
+      id: 3,
+      productName: "Scottish Fold",
+      price: 20.29,
+      productImage: product3,
+    },
+    {
+      id: 4,
+      productName: "Exotic Shorthair",
+      price: 23.29,
+      productImage: product4,
+    },
+    {
+      id: 5,
+      productName: "Ankara kedisi ",
+      price: 18.99,
+      productImage: product5,
+    },
+    {
+      id: 6,
+      productName: "Maine Coon ",
+      price: 26.99,
+     productImage: product6,
+    },
+    {
+      id: 7,
+      productName: "Ragdoll Kedisi ",
+      price: 20.0,
+     productImage: product7,
+    },
+    {
+      id: 8,
+      productName: "Munchkin",
+      price: 21.49,
+     productImage: product8,
+    },
+  ];
